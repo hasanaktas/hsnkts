651b15bb8b24a06503e71b5351bfcb6e04ef7596
hasanaktas <hasanaktas95@gmail.com>
2020-04-26 11:12:48 +0300

    fixed

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,1 @@
-export { default as SkillCard } from './cardSkill'
+export { default as SkillCard } from './skillCard'
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,30 +1,9 @@
 import React from 'react'
 import Container from '@material-ui/core/Container'
-import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
-import Button from '@material-ui/core/Button'
-import ProTip from '../components/ProTip'
-import Link from '../components/Link'
-import Copyright from '../components/Copyright'
+
 import { MainLayout } from '../layouts'
-import { useDispatchLocalization, useLocalization } from '../localization'
 export default function About() {
-  const localization = useLocalization()
-  return (
-    <Container maxWidth='sm'>
-      <Box my={4}>
-        <Typography variant='h4' component='h1' gutterBottom>
-          {localization.home}
-        </Typography>
-        <Button variant='contained' color='primary'>
-          Go to the main page
-        </Button>
-        <Button color='primary'>Go to the main page</Button>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  )
+  return <Container maxWidth='sm'></Container>
 }
 
 About.layout = MainLayout
